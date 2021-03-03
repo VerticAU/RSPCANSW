d941ac2c789542015449b406818250c5d31fa7ec
dlisovsky <>
2021-03-03 13:04:28 +0000

    [ClickDeploy] RSPCA NSW DEV BACKUP

--- aura/PayPalSubscriptionQA/PayPalSubscriptionQAController.js
+++ aura/PayPalSubscriptionQA/PayPalSubscriptionQAController.js
@@ -1,5 +1,27 @@
-/**
- * Created by macos on 3/2/21.
- */
+({
+    handleInit: function(cmp, event, helper){
+        helper.execute(cmp, 'vertic_SOQLProc',
+            {
+                SOQL: 'SELECT CreatedById FROM Batch__c WHERE Id = \'' + cmp.get('v.recordId') + '\' LIMIT 1'
+            }
+        ).then(function (response) {
+            if (response.isValid) {
+                if($A.get("$SObjectType.CurrentUser.Id") === response.dto.records[0].CreatedById){
+                    cmp.set('v.message', 'Batch can not be approved by user who has created it!');
+                    cmp.set('v.disabled', true);
+                } else {
+                    cmp.set('v.message', 'Please note, this action will update Batch Approved By field. Press \'Approve\' to continue.');
+                    cmp.set('v.disabled', false);
+                }
+            }
+        })
+    },
 
-({});
+    handleApprove: function (cmp, event, helper) {
+        helper.approve(cmp, helper);
+    },
+
+    handleCancelClick: function(cmp, event, helper){
+        helper.closeModal(cmp);
+    }
+})
