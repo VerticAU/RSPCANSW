72f6dc688f3c1fcdb21a0c4e164c735c45c92f57
dlisovsky <>
2021-05-18 14:02:43 +0000

    [ClickDeploy] RSPCA NSW DEV BACKUP

--- aura/EducationCentreCalendar/EducationCentreCalendarController.js
+++ aura/EducationCentreCalendar/EducationCentreCalendarController.js
@@ -1,8 +1,16 @@
 ({
-	doInit : function(component, event, helper) {
-        component.set("v.myPrototypeReservation", {
-            sobjectType: 'B25__Reservation__c',
-            B25__Staff__c: component.get('v.recordId')
-        });
-	}
+  doInit: function (cmp, event, helper) {
+    var recordId = cmp.get('v.recordId');
+    cmp.set('v.myFilters', [
+        { 'fieldName': 'B25__Is_Active__c', 'operator': '=', 'value': 'true'},
+        {
+            type: 1,
+            logicalOperator: 'OR',
+            subFilters: [
+                { 'fieldName': 'B25__Parent__c', 'operator': '=', 'value': 'a301y0000001PK7AAM'},
+                { 'fieldName': 'B25__Parent__c', 'operator': '=', 'value': 'a301y0000001PrwAAE'}       
+            ]
+        }
+    ]);
+  }
 })
